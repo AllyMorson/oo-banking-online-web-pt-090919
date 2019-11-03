3da69e4a67c3ccde39ef82bc2ea68acffa8a6055
Ally Morson <ally.morson@gmail.com>
2019-11-03 20:59:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ def valid?
 end 
 
 def execute_transaction
-  if valid? && sender.balace > amount && self.status == "pending"
+  if valid? && sender.balance > amount && self.status == "pending"
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete"
